283b411f3df4c24ac5f5ec949b22fc3930017cb6
Ayush Singhal <ayushsinghal2801@gmail.com>
2025-05-26 16:36:38 +0530

    added login

--- app.js
+++ app.js
@@ -14,13 +14,8 @@ app.set("view engine", "ejs");
 app.use("/api/auth", authRoutes);
 
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
 
-app.get("/about", (req, res) => {
-  res.send("About Page");
-});
+
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
--- routes/auth.js
+++ routes/auth.js
@@ -1,29 +1,68 @@
-const router = require('express').Router();
-const User = require('../models/user');
-const ejs = require('ejs');
+const router = require("express").Router();
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
-router.post('/register',async (req,res)=>{
-    try {
-        const {username,email,password} = req.body;
-        const user = new User({username,email,password});
-        await user.save().then(()=>{
-            res.status(200).json({
-                message:"User created successfully",
-                user
-            })
+router.post("/register", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const existingUser = await User.findOne({
+      $or: [{ email }, { username }],
+    });
+
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "Username or email already exists.",
+      });
+    }
+
+    const user = new User({ username, email, password: hashedPassword });
+
+    await user.save().then(() => {
+      res.status(200).json({
+        message: "User created successfully",
+        user,
+      });
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "something unexpected happened",
+      error: error.message,
+    });
+  }
+});
+
+router.get("/register", (req, res) => {
+  res.render("register");
+});
+
+router.post('/login',async (req,res)=>{
+    const {email,password} = req.body;
+    const user = await User.findOne({email});
+    if(!user){
+        return res.status(400).json({
+            message:"User not found"
         })
+    }
+    
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-        
-    } catch (error) {
-            res.status(400).json({
-                message: "User creation failed",
-                error: error.message
-            });
-        }
+    if(user.password === hashedPassword){
+        return res.status(200).json({
+            message:"Login successful",
+            user
+        })
+    }
+
 })
 
-router.get('/register',(req,res)=>{
-    res.render('register');
+router.get('/login',async (req,res)=>{
+    res.render('login'); 
+    
+
 })
 
 module.exports = router;
